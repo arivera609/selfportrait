753f4e3c56afe08eba0fecb6724621b8cc781843
arivera609 <angel.rivera@f1006-09-34181.bmcc.cuny.edu>
2017-09-05 20:33:06 -0400

    initial commit

---
+++ sketch.js
@@ -0,0 +1,66 @@
+function setup() {
+    createCanvas(640, 460);
+}
+function draw() {
+    background("white");
+    noStroke();
+    
+    
+    
+    fill("#ffe7d6");
+    ellipse(200,200,200,200);
+    
+    fill("white");
+    ellipse(160,170,30,25);
+    
+    fill("white");
+    ellipse(240,170,30,25);
+    
+     fill("black");
+    ellipse(240,170,10,15);
+    
+    fill("black");
+    ellipse(160,170,10,15);
+    
+
+    
+     fill("#c9a9a9");
+    rect(195,185,10,50);
+    
+    fill("black");
+    rect(175,250,50,5);
+    
+     fill("black");
+    rect(214,140,50,5);
+    
+      fill("black");
+    rect(130,140,50,5);
+    
+     fill("black");
+    rect(130,70,140,60);
+    
+    fill("#ffe7d6");
+    rect(190,260,20,60);
+    
+     fill("#5d00ff");
+    rect(130,310,150,150);
+    
+    fill("#ffe7d6");
+    rect(280,310,40,150);
+    
+    fill("#ffe7d6");
+    rect(90,310,40,150);
+    
+    
+    
+     fill("#5d00ff");
+    rect(90,310,50,80);
+    
+    fill("#5d00ff");
+    rect(270,310,50,80);
+    
+     
+    
+    
+    
+}
